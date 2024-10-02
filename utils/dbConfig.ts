4840d145126062059573eed78ba9044b67bc66ec
James418-alt <ebifegha123@gmail.com>
2024-10-02 12:21:27 +0100

    dist folder created

--- utils/dbConfig.ts
+++ utils/dbConfig.ts
@@ -1,6 +1,7 @@
 import { clear } from "console";
 import { connect } from "mongoose";
-const url = "mongodb://localhost:27017/passportDb";
+const url =
+  "mongodb+srv://ebifegha123:ebifegha123@gomenticode.1l9lbmv.mongodb.net/passportDB?retryWrites=true&w=majority&appName=GomentiCode";
 export const dbConfig = async () => {
   await connect(url).then(() => {
     clear(), console.log("Server up");
